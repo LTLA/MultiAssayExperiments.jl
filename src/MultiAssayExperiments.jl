9a329de5ad8bc2f9dd9dafdef4fce0cc0f4798b3
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2022-02-16 10:52:09 -0800

    Added per-experiment getters and setters.

--- src/MultiAssayExperiments.jl
+++ src/MultiAssayExperiments.jl
@@ -2,5 +2,6 @@ module MultiAssayExperiments
 include("class.jl")
 include("miscellaneous.jl")
 include("exampleobject.jl")
+include("extract.jl")
 include("getters.jl")
 end # module
--- src/getters.jl
+++ src/getters.jl
@@ -1,4 +1,4 @@
-export experiments, sampledata, samplemap, metadata, setexperiments!, setsampledata!, setsamplemap!, setmetadata!
+export experiments, experiment, sampledata, samplemap, metadata, setexperiments!, setexperiment!, setsampledata!, setsamplemap!, setmetadata!
 import DataStructures
 import DataFrames
 import SummarizedExperiments
@@ -49,6 +49,158 @@ julia> collect(keys(experiments(x)))
 """
 function setexperiments!(x::MultiAssayExperiment, value::DataStructures.OrderedDict{String,SummarizedExperiments.SummarizedExperiment})
     x.experiments = value;
+    return x
+end
+
+"""
+    experiment(x[, i]; sampledata = false)
+
+Extract the specified `SummarizedExperiment` from a `MultiAssayExperiment` `x`.
+`i` may be a positive integer no greater than the number of experiments in `x`,
+or a string specifying the name of the desired experiment.
+If `i` is not specified, it defaults to the first experiment in `x`.
+
+If `sampledata = true`, we attempt to add the sample data of `x` to the `coldata` of the returned `SummarizedExperiment`.
+This is done by subsetting `sampledata(x)` based on sample mapping to the columns of the returned `SummarizedExperiment`.
+Note that this may not be possible if there are columns of the `SummarizedExperiment` that are not present in the sample mapping,
+in which case an error is raised - use [`dropunused`](@ref) in that case.
+If there are columns in the `sampledata(x)` and the `coldata` of the `SummarizedExperiment` with the same name but different values,
+the former are omitted with a warning.
+
+Note that, if `sampledata = true`, the returned `SummarizedExperiment` will be a copy of the relevant experiment in `x`.
+If `false`, the returned object will be a reference.
+
+# Examples
+```jldoctest
+julia> using MultiAssayExperiments;
+
+julia> x = exampleobject();
+
+julia> experiment(x)
+100x10 SummarizedExperiments.SummarizedExperiment
+  assays(3): foo bar whee
+  rownames: Gene1 Gene2 ... Gene99 Gene100
+  rowdata(2): name Type
+  colnames: foo1 foo2 ... foo9 foo10
+  coldata(3): name Treatment Response
+  metadata(1): version
+
+julia> experiment(x, 1); # same result
+
+julia> experiment(x, "foo");
+
+julia> experiment(x, "foo", sampledata = true) # add sample data
+100x10 SummarizedExperiment
+  assays(3): foo bar whee
+  rownames: Gene1 Gene2 ... Gene99 Gene100
+  rowdata(2): name Type
+  colnames: foo1 foo2 ... foo9 foo10
+  coldata(4): name Treatment Response disease
+  metadata(1): version
+```
+"""
+function experiment(x::MultiAssayExperiment; sampledata = false)
+    return experiment(x, 1, sampledata = sampledata)
+end
+
+function safely_add_columns!(host, other, experiment)
+    for n in names(other)
+        if (n == "name") 
+            continue # skipping the name.
+        end
+
+        src = other[!,n]
+
+        if n in names(host)
+            if src != host[!,n]
+                @warn "omitting sample data column '" * n * "' with conflict in column data of experiment '" * experiment * "'" 
+            end
+            continue
+        end
+
+        host[!,n] = src
+    end
+end
+
+function experiment(x::MultiAssayExperiment, i::Int; sampledata = false)
+    counter = 1
+    for (key, val) in experiments(x)
+        if (i == counter)
+            if !sampledata
+                return val
+            else
+                sd = extractsampledata(x, key, coldata(val)[!,"name"])
+                val2 = copy(val)
+                cd = copy(SummarizedExperiments.coldata(val2))
+                safely_add_columns!(cd, sd, key)
+                SummarizedExperiments.setcoldata!(val2, cd)
+                return val2
+            end
+        end
+        counter += 1
+    end
+    throw(BoundsError("experiment " * string(i) * " is out of range (" * length(experiments(x)) * " experiments available)"))
+end
+
+function experiment(x::MultiAssayExperiment, i::String; sampledata = false)
+    val = x.experiments[i]
+    if !sampledata
+        return val
+    else
+        sd = extractsampledata(x, i, SummarizedExperiments.coldata(val)[!,"name"])
+        val2 = copy(val)
+        cd = copy(SummarizedExperiments.coldata(val2))
+        safely_add_columns!(cd, sd, i)
+        SummarizedExperiments.setcoldata!(val2, cd)
+        return val2
+    end
+end
+
+"""
+    setexperiment!(x[, i], value)
+
+Set experiment `i` in `MultiAssayExperiment` `x` to the `SummarizedExperiment` `value`.
+This returns a reference to the modified `x`.
+
+`i` may be a positive integer, in which case it should be no greater than the length of `experiments(x)`.
+It may also be a string specifying a new or existing experiment in `x`.
+If omitted, we set the first experiment by default.
+
+# Examples
+```jldoctest
+julia> using MultiAssayExperiments;
+
+julia> x = exampleobject();
+
+julia> size(experiment(x, 2))
+(50, 8)
+
+julia> val = experiment(x);
+
+julia> setexperiment!(x, 2, val);
+
+julia> size(experiment(x, 2))
+(100, 10)
+```
+"""
+function setexperiment!(x::MultiAssayExperiment, value::SummarizedExperiments.SummarizedExperiment)
+    return setexperiment!(x, 1, value)
+end
+
+function setexperiment!(x::MultiAssayExperiment, i::Int, value::SummarizedExperiments.SummarizedExperiment)
+    counter = 1
+    for key in keys(experiments(x))
+        if i == counter
+            x.experiments[key] = value
+            return x
+        end
+        counter += 1
+    end
+    throw(BoundsError("experiment " * string(i) * " is out of range (" * length(experiments(x)) * " experiments available)"))
+end
+
+function setexperiment!(x::MultiAssayExperiment, i::String, value::SummarizedExperiments.SummarizedExperiment)
+    x.experiments[i] = value
     return x
 end
 
